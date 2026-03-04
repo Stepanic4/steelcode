e189326d8a95c863ef99c51b0fac02f2c5bc66aa
Ivan <Zolotukhin.ivan.1@gmail.com>
2026-03-04 06:49:06 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Burger from "./Burger";
 
 export default function Header() {
     return (
-        <header className="flex items-center justify-between px-6 py-4 border-b bg-[#c4c4c4]">
+        <header className="flex items-center justify-between px-6 py-4 border-b bg-[#000000]">
             <Logo />
             <Burger />
         </header>
