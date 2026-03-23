4b3ba4456d93587fba4d4bd0cc2360bd8330c24b
Ivan <Zolotukhin.ivan.1@gmail.com>
2026-03-23 20:38:53 +0100

    change transperent text in footer

--- src/components/structure/Footer.tsx
+++ src/components/structure/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="p-6 border-t border-white/70 bg-zinc-900/80 text-center text-sm font-mono">
+        <footer className="p-6 border-t border-white/70 bg-zinc-900/80 text-center text-sm text-white/50 font-mono">
             &copy; {new Date().getFullYear()} STEELCODE.CZ — ALL RIGHTS RESERVED
         </footer>
     );
