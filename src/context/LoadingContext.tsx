ccc54e0b561f1b6d995556191f620b1fe3a2e1e3
Ivan <Zolotukhin.ivan.1@gmail.com>
2026-04-22 20:38:59 +0200

    change value

--- src/context/LoadingContext.tsx
+++ src/context/LoadingContext.tsx
@@ -1,18 +1,34 @@
-'use client'
-import { createContext, useContext, useState } from "react";
+"use client";
+import {
+  createContext,
+  useContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
 
-const LoadingContext = createContext({
-    isReady: false,
-    setIsReady: (value: boolean) => {}
-});
+type LoadingContextType = {
+  isReady: boolean;
+  setIsReady: Dispatch<SetStateAction<boolean>>;
+};
 
-export const LoadingProvider = ({ children }: { children: React.ReactNode }) => {
-    const [isReady, setIsReady] = useState(false);
-    return (
-        <LoadingContext.Provider value={{ isReady, setIsReady }}>
-            {children}
-        </LoadingContext.Provider>
-    );
+const LoadingContext = createContext<LoadingContextType>({
+  isReady: false,
+  setIsReady: () => {},
+} as LoadingContextType);
+
+export const LoadingProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [isReady, setIsReady] = useState(false);
+
+  return (
+    <LoadingContext.Provider value={{ isReady, setIsReady }}>
+      {children}
+    </LoadingContext.Provider>
+  );
 };
 
 export const useLoading = () => useContext(LoadingContext);
